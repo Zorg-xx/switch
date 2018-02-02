e1f6745f685f0a3468ca7f978472e832e9e09363
yohann <kid_project@yahoo.fr>
2018-02-02 11:31:07 +0100

    Maj Rond et création abstract class Forme

---
+++ src/modele/Forme.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ColorSwitchApp.modele;
+
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author y
+ */
+public class Forme {
+    protected final static Color violet=Color.PURPLE;
+    protected final static Color jaune=Color.YELLOW;
+    protected final static Color vert=Color.GREEN;
+    protected final static Color rouge=Color.RED;
+    
+    protected Double x;
+    protected Double y;
+    
+    public Forme(Double _x,Double _y){
+        x=_x;
+        y=_y;
+    }
+    //360 rotation horaire, -360 rotation anti-horaire
+    //vitesse 5
+    public void tourne(Shape p, int sensRotation, int vitesse){
+        final Rotate rotationTransform = new Rotate(0, x, y);
+        p.getTransforms().add(rotationTransform);
+        final Timeline rotationAnimation = new Timeline();
+        rotationAnimation.getKeyFrames().add(new KeyFrame(Duration.seconds(vitesse)
+                ,new KeyValue(rotationTransform.angleProperty()
+                ,sensRotation
+                ,Interpolator.LINEAR)));
+        rotationAnimation.setCycleCount(Animation.INDEFINITE);
+        rotationAnimation.setAutoReverse(false);
+        rotationAnimation.play();
+        
+    }
+}
---
+++ src/modele/Rond.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ColorSwitchApp.modele;
+
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcBuilder;
+import javafx.scene.shape.ArcType;
+
+/**
+ *
+ * @author y
+ */
+
+/* exemple d'utilisation
+Rond r=new Rond(250,250,50,10,95,80);
+
+ArrayList liste = r.getListeArc();
+for(Object a: liste){
+    r.tourne((Arc)a,360,5);
+    root.getChildren().add((Arc)a);
+    }
+*/
+//rond de base
+//radius 50
+//grosseur trait 10
+//angleDepart 95, 185, 275, 365
+//longueurArc 80
+public class Rond extends Forme{
+    
+    int radius;
+    int grosseurTrait;
+    int angleDepart;
+    int longueurArc;
+    ArrayList<Arc>listeArc;
+    
+    public Rond(Double _x,Double _y, int _radius,int _grosseurTrait, int _angleDepart, int _longueurArc){
+        super(_x,_y);
+        radius=_radius;
+        grosseurTrait=_grosseurTrait;
+        angleDepart=_angleDepart;
+        longueurArc=_longueurArc;
+        listeArc=new ArrayList<Arc>();
+        creerArc(radius,grosseurTrait,violet,angleDepart,longueurArc);
+        creerArc(radius,grosseurTrait,jaune,angleDepart+90,longueurArc);
+        creerArc(radius,grosseurTrait,vert,angleDepart+(90*2),longueurArc);
+        creerArc(radius,grosseurTrait,rouge,angleDepart+(90*3),longueurArc);
+    }
+    
+    public ArrayList getListeArc(){
+        return listeArc;
+    }
+    
+    private Arc creerArc(int radius,int grosseurTrait,Color couleur,int angleDepart,int longueurArc){
+        Arc a = ArcBuilder.create()
+            .type(ArcType.OPEN)
+            .centerX(x)
+            .centerY(y)
+            .radiusX(radius)
+            .radiusY(radius)
+            .startAngle(angleDepart)
+            .length(longueurArc)
+            .fill(null)
+            .stroke(couleur)
+            .strokeWidth(grosseurTrait)
+            .build();
+        listeArc.add(a);
+        return a;
+    }
+}
