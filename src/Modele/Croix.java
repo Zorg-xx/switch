42c217a9edb33661af973fb4a008decc56be130d
trinitie8 <al@al-VirtualBox>
2018-02-05 17:49:50 +0100

    une croix

---
+++ src/Modele/Croix.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.LineBuilder;
+
+public class Croix extends Forme{
+    
+    int grosseurTrait;
+    int largeur;
+    double decalage;
+    ArrayList<Line>listeLigneC;
+    
+    public Croix(double _x,double _y, int _grosseurTrait, int _largeur, int _decalage){
+        super(_x,_y);
+        grosseurTrait=_grosseurTrait;
+        largeur=_largeur;
+        decalage=_decalage;
+        listeLigneC=new ArrayList<Line>();
+        creerLigneC(grosseurTrait,violet,_x,_y,_x,_y+largeur);
+        creerLigneC(grosseurTrait,jaune,_x,_y,_x+largeur,_y);
+        creerLigneC(grosseurTrait,vert,_x,_y,_x,_y-largeur);
+        creerLigneC(grosseurTrait,rouge,_x,_y,_x-largeur,_y);
+    }
+    
+    public ArrayList getListeLigneC(){ 
+        return listeLigneC;
+    }
+    
+    private Line creerLigneC(int grosseurTrait,Color couleur, double _departX,double _departY, double _finX, double _finY){
+        Line l = LineBuilder.create()
+            .startX(_departX)
+            .startY(_departY)
+            .endX(_finX)
+            .endY(_finY)
+            .fill(null)
+            .stroke(couleur)
+            .strokeWidth(grosseurTrait)
+            .build();
+        listeLigneC.add(l);
+        return l;
+    }
+}
