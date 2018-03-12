0bc84373de41d81c4846b1e215d6090b71d1393b
trinitie8 <trinitie8@gmail.com>
2018-03-12 18:03:43 +0100

    ajout forme quitter

--- src/controleur/ControleurChangeVue.java
+++ src/controleur/ControleurChangeVue.java
@@ -1,3 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+    
 package controleur;
 
 
@@ -5,9 +14,11 @@ import vue.Vue2;
 import vue.Vue1;
 import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
@@ -19,23 +30,60 @@ public class ControleurChangeVue implements EventHandler{
         ps=_ps;
     }
     
-    @Override
     public void handle(Event event) {
         
         Object o=event.getSource();
         
-        if(o instanceof Circle )
+        if(o instanceof Circle ){
             if(((Circle) o).getId().equalsIgnoreCase("start")){
                 Vue2 v=new Vue2(ps);
             }
-        if(o instanceof Polygon)
+            if(((Circle) o).getId().equalsIgnoreCase("restart")){
+                Vue2 v=new Vue2(ps);
+            }
+        }
+            
+        if(o instanceof Polygon){
             if(((Polygon) o).getId().equalsIgnoreCase("start")){
                 Vue2 v=new Vue2(ps);
             }
-        if(o instanceof Group)
-            if(((Group) o).getId().equalsIgnoreCase("rejouer")){
+            if(((Polygon) o).getId().equalsIgnoreCase("quit")){
+                System.exit(0);
+            }
+            if(((Polygon) o).getId().equalsIgnoreCase("restart")){
                 Vue2 v=new Vue2(ps);
             }
-
+        }
+        
+        if (o instanceof Rectangle){
+            if(((Rectangle) o).getId().equalsIgnoreCase("quit")){
+                System.exit(0);
+            }
+        }
+        
+        if (o instanceof Arc){
+            if(((Arc) o).getId().equalsIgnoreCase("restart")){
+                Vue2 v=new Vue2(ps);
+            }
+        }
+            
+        if(o instanceof Button)
+            if(((Button) o).getId().equalsIgnoreCase("bouton2")){
+                Vue1 v=new Vue1(ps);
+            }
+        
+        /*        
+        Button b=(Button)event.getSource();
+             
+        if(b.getId().equalsIgnoreCase("bouton1")){
+            Vue2 v=new Vue2(ps);
+        }
+        
+        if(b.getId().equalsIgnoreCase("bouton2")){
+            Vue1 v=new Vue1(ps);
+        }   
+        */
     } 
 }
+    
+
